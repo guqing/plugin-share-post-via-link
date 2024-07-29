d844caf4b6f80b9c1c6069bf06f30925fecb6fc8
guqing <i@guqing.email>
2024-07-29 17:45:29 +0800

    feat: refine ui

---
+++ ui/src/api/index.ts
@@ -0,0 +1,8 @@
+import {axiosInstance} from "@halo-dev/api-client";
+import {PostShareLinkV1alpha1Api} from "./generated";
+
+const postShareApiClient = {
+  postShare: new PostShareLinkV1alpha1Api(undefined, "", axiosInstance),
+};
+
+export {postShareApiClient};
---
+++ ui/src/views/SharedPostLinkList.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+import {IconBookRead, VCard, VPageHeader,} from "@halo-dev/components";
+</script>
+
+<template>
+  <VPageHeader title="文章分享管理">
+    <template #icon>
+      <IconBookRead class="mr-2 self-center"/>
+    </template>
+  </VPageHeader>
+
+  <div class="m-0 md:m-4">
+    <VCard :body-class="['!p-0']">
+    </VCard>
+  </div>
+</template>
+
+<style scoped>
+
+</style>

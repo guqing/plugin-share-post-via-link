7b906c0c6d872e3a82a0b3f899b3b9c4a64b9a88
Ryan Wang <i@ryanc.cc>
2024-07-30 12:02:17 +0800

    feat: add creation modal and shared link list
    Signed-off-by: Ryan Wang <i@ryanc.cc>

---
+++ ui/src/components/PostShareLinkCreationDropdownItem.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+import type { ListedPost } from "@halo-dev/api-client";
+import { VDropdownItem } from "@halo-dev/components";
+import { ref } from "vue";
+import PostShareLinkCreationModal from "./PostShareLinkCreationModal.vue";
+
+withDefaults(
+  defineProps<{
+    post: ListedPost;
+  }>(),
+  {},
+);
+
+const visible = ref(false);
+</script>
+
+<template>
+  <div>
+    <VDropdownItem @click="visible = true"> 创建分享链接 </VDropdownItem>
+    <PostShareLinkCreationModal
+      v-if="visible"
+      :post="post.post"
+      @close="visible = false"
+    />
+  </div>
+</template>
---
+++ ui/src/components/PostShareLinkViewResultModal.vue
@@ -0,0 +1,84 @@
+<script lang="ts" setup>
+import { postShareApiClient } from "@/api";
+import type { PostShareLink } from "@/api/generated";
+import { Toast, VButton, VLoading, VModal, VSpace } from "@halo-dev/components";
+import { useQuery } from "@tanstack/vue-query";
+import { useClipboard } from "@vueuse/core";
+import { ref, toRefs } from "vue";
+
+const props = withDefaults(
+  defineProps<{
+    postShareLink: PostShareLink;
+  }>(),
+  {},
+);
+
+const { postShareLink } = toRefs(props);
+
+const emit = defineEmits<{
+  (event: "close"): void;
+}>();
+
+const modal = ref<InstanceType<typeof VModal> | null>(null);
+
+const { copy } = useClipboard({ legacy: true });
+
+const { data, isLoading } = useQuery({
+  queryKey: ["plugin:share-post-via-link:get-by-name", postShareLink],
+  queryFn: async () => {
+    const { data } = await postShareApiClient.postShare.getPostShareLink({
+      name: postShareLink.value.metadata.name,
+    });
+
+    if (!data.status?.permalink) {
+      throw new Error("Failed to get permalink");
+    }
+
+    return data;
+  },
+  retry: 5,
+});
+
+function handleCopyLink() {
+  copy(data.value?.status?.permalink || "");
+  Toast.success("复制成功");
+  modal.value?.close();
+}
+</script>
+
+<template>
+  <VModal
+    ref="modal"
+    :centered="false"
+    mount-to-body
+    :width="500"
+    title="创建分享链接成功"
+    @close="emit('close')"
+  >
+    <div>
+      <VLoading v-if="isLoading" />
+      <div v-else class="flex flex-col space-y-2">
+        <span class="text-sm text-gray-900"> 分享链接为： </span>
+        <a
+          class="text-indigo-500 hover:text-indigo-600 text-sm"
+          :href="data?.status?.permalink"
+          target="_blanks"
+        >
+          {{ data?.status?.permalink }}
+        </a>
+      </div>
+    </div>
+    <template #footer>
+      <VSpace>
+        <VButton
+          :disabled="!data?.status?.permalink"
+          type="secondary"
+          @click="handleCopyLink"
+        >
+          复制
+        </VButton>
+        <VButton @click="modal?.close()">关闭</VButton>
+      </VSpace>
+    </template>
+  </VModal>
+</template>
